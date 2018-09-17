687e5db2ebcb79e7742b9c9f82b9c33977dcbc81
Jessica <sea.jessicac@gmail.com>
2018-09-17 19:55:43 -0400

    first commit

---
+++ src/scripts/app.js
@@ -0,0 +1,18 @@
+const hello = 'World';
+console.log(hello);
+
+// const svg = document.getElementById("svgPath");
+// const length = svg.getTotalLength();
+
+// svg.style.strokeDasharray = length;
+
+// svg.style.strokeDashoffset = length;
+
+// window.addEventListener('scroll', function () {
+//   const scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+//   const draw = length * scrollpercent;
+
+//   svg.style.strokeDashoffset = length - draw;
+// });
+
